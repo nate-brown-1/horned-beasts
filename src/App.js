ed9f478f91dd8832ca780e49a4bb9e701b3aa9f6
Nate Brown <nathan.d.brown@gmail.com>
2023-02-27 23:48:26 -0800

    commit for submission before 12am

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 import './App.css';
 
-class App extends React.Component{
+class App extends React.Component {
 
   render() {
     return (
       <>
-        <header>
-          <h1>Horned Beasts</h1>
-        </header>
-        <main>Main</main>
-        <footer>Footer</footer>
+        <Header/>
+        <Main/>
+        <Footer/>
       </>
       );
   }
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <>
+        <footer>Written by Nate Brown</footer>
+      </>
+      );
+  }
+}
+
+export default Footer;
