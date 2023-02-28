f129973709c683f29742f674fe12cd58cd90bfa3
Nate Brown <nathan.d.brown@gmail.com>
2023-02-28 11:42:22 -0800

    lab2 first commit

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,24 +1,39 @@
 import React from 'react';
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0
+    }
+  }
+
+  handleLikes = () => {
+    this.setState({
+      likes:this.state.likes + 1,
+    });  
+  }
+
   render() {
     return (
       <>
         <article>
-        <h2>
-          {this.props.title}
-        </h2>
-        <img
-          src={this.props.imageUrl}
-          alt={this.props.imageAlt}
-          title={this.props.imageTitle}>
-        </img>
-        <p>
-          {this.props.description}
-        </p>
+          <h2>
+            {this.props.title}
+          </h2>
+          <img
+            src={this.props.imageUrl}
+            alt={this.props.imageAlt}
+            title={this.props.imageTitle}>
+          </img>
+          <p onClick={this.handleLikes}>Like</p>
+          <p>{this.state.likes}</p>
+          <p>
+            {this.props.description}
+          </p>
         </article>
       </>
-      );
+    );
   }
 }
 
