b56b740b9eddcded6cabb34d756bfc61a5268a5f
Nate Brown <nathan.d.brown@gmail.com>
2023-02-28 21:49:25 -0800

    working except column display

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 class HornedBeast extends React.Component {
   constructor(props) {
@@ -17,25 +18,27 @@ class HornedBeast extends React.Component {
   render() {
     return (
       <>
-        <article>
-          <h2>
-            {this.props.title}
-          </h2>
-          <img
-            src={this.props.imageUrl}
-            alt={this.props.imageAlt}
-            title={this.props.imageTitle}>
-          </img>
-          <p onClick={this.handleLikes}>Like</p>
-          <p>💌 {this.state.likes}</p>
-          <p>
-            {this.props.description}
-          </p>
-        </article>
+        <Card className="cardClass">
+          <Card.Img
+            variant="top"
+            src={this.props.image_url}
+            alt={this.props.description}
+            title={this.props.image_title}
+          />
+          <Card.Body>
+            <Card.Title>
+              <h2>{this.props.title}</h2>
+            </Card.Title>
+            <Card.Text>
+              <p>{this.props.description}</p>
+              <p onClick={this.handleLikes}>Like</p>
+              <p>💌 {this.state.likes}</p>
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </>
     );
   }
 }
 
 export default HornedBeast;
-
